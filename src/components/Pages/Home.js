eb9f8123b19e8190480f264a7ad891046644969a
RohitSingh104 <rksingh9173@gmail.com>
2023-03-27 16:11:57 +0530

    3rd commit

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,12 +1,9 @@
 import { Link } from "react-router-dom";
-
+import "./CSS/home.css";
 const Home = () => {
   return (
-    <div>
-      <h2>Home Page</h2>
-      <Link to="About"> See about Page</Link>
-      <Link to="Project"> See project Page</Link>
-      <Link to="Contact"> See Contact Page</Link>
+    <div className="home-page">
+ 
     </div>
   );
 };
