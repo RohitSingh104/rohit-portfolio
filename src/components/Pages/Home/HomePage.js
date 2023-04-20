202c8659ac0a43ab124a29417223733f3a332564
RohitSingh104 <rksingh9173@gmail.com>
2023-04-20 05:40:01 +0530

    left section

--- src/components/Pages/Home/HomePage.js
+++ src/components/Pages/Home/HomePage.js
@@ -1,7 +1,15 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
 
 const HomePage = () => {
+  const iconStyle = {
+    width:'30px',
+    height:'30px'
+  }
   return (
-    <div className="HomePage-container">
+    <div className="homePage-container">
       <div className="homepage-leftside">
         <h2>Hello</h2>
         <span></span>
@@ -10,13 +18,13 @@ const HomePage = () => {
         <br />
         <h2 className="role">Software Engineer</h2>
         <div className="buttons">
-          {/* <button onclick="window.location.href='https://github.com/RohitSingh104';">
-
-          </button> */}
+          <GitHubIcon style={iconStyle} onClick={() => window.open('https://github.com/RohitSingh104')} target="_blank"/> 
+          <LinkedInIcon style={iconStyle} onClick={() => window.open('https://www.linkedin.com/in/rohit-singh-478476153/')} target="_blank"/> 
+          <TwitterIcon style={iconStyle} onClick={() => window.open('https://twitter.com/rk_singh_01')} target="_blank"/>
         </div>
       </div>
       <div className="homepage-rightside">
-      
+        <div className='home-image'></div>
       </div>
     </div>
   );
