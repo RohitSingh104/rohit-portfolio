87e19c15bf69a1ce4f4f2af9f27fc651ba96cea0
RohitSingh104 <rksingh9173@gmail.com>
2023-04-25 19:38:39 +0530

    about change

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,9 +1,42 @@
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="inner-box">
+        <div className="ab-left-section">
+          <h2>About Me</h2>
+          <p>
+            I'm a self-taught Front End developer from India.
+            <br />I build websites with a focus on providing the experience
+            <br /> for everyone using them and responsiveness. <br />
+            Curious to learn more about developing
+            <br /> scalable distribution systems, love problem solving, and
+            <br /> care about writing and maintainable code. <br />A responsive
+            design makes your website accessible to all users,
+            <br /> regardless of their devices.
+          </p>
 
+          <ul>
+            <h2>Skills</h2>
+            <div className="skills">
+              <div className="list-left">
+                <li>HTML5/CSS3</li>
+                <li>JavaScript</li>
+                <li>Bootstrap</li>
+                <li>React JS</li>
+              </div>
 
-const About = () =>{
-    return(
-        <h2>About Page</h2>
-    )
-}
+              <div className="list-right">
+                <li>Material UI</li>
+                <li>Redux</li>
+                <li>Git/GitHub</li>
+              </div>
+            </div>
+          </ul>
+        </div>
+        <div className="ab-right-section"></div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
