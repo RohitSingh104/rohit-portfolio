aefdafe53f691444fe47965e322705ab22509dcd
RohitSingh104 <rksingh9173@gmail.com>
2023-04-28 11:56:28 +0530

    contact added

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,8 +1,22 @@
-
-const Contact = () =>{
-    return(
-        <h2>Contact Page</h2>
-    )
-}
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <div className="contact-text">
+        <h2>Contact Me</h2>
+        <p>
+          I am looking for opportunities for frontend development role. <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;I am ready for any part-time
+          ,full-time opportunities. <br />{" "}
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We
+          can discuss in detail anytime. <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Connect
+          me via email or call.
+        </p>
+        <h3>Email : rksingh9173@gmail.com</h3>
+        <h3>&nbsp;&nbsp;&nbsp;Call : 9173716191</h3>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
